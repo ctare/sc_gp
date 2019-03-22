63cf5e930a5f7f7d8647d8bb9f69d11c5ae0b2a8
ctare <sankaku900622@yahoo.co.jp>
2019-03-23 00:05:46 +0900

    init

---
+++ gp_client.py
@@ -0,0 +1,28 @@
+from pylib.sc_util import *
+from pylib.gp_util import *
+from pylib.hgp import *
+
+#%%
+globals().update(load_data("jordan_ok/"))
+
+#%%
+GlobalStat.reset()
+creator_init()
+phase_init()
+
+#%%
+def eval_f(value):
+    print("end", end=" ")
+    for i in range(3):
+        time.sleep(0.001)
+        print(i, end=" ")
+    # return value ** 2
+    return len(value)
+    # return eval_simple_tree(value)
+
+
+#%%
+print("start")
+
+while True:
+    working(eval_f)
