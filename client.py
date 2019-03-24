b635b7add7b0030c8c7968b0ddb7fa6717063c13
ctare <sankaku900622@yahoo.co.jp>
2019-03-24 15:50:39 +0900

    phase

--- client.py
+++ client.py
@@ -1,14 +1,19 @@
 from pylib.sc_util import *
 import time
 from tqdm import tqdm
+import json
 
 #%%
 def eval_f(value):
     # print("end", end=" ")
+    data = json.loads(value)
+    string = data["string"]
+    phase = data["phase"]
+    print("phase", phase)
     for i in tqdm(range(10)):
         time.sleep(0.01)
         # print(i, end=" ")
-    return len(value)
+    return len(string)
 
 
 #%%
