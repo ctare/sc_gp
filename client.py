286c45de57b1268542ea810f300e632b2e4769af
ctare <sankaku900622@yahoo.co.jp>
2019-03-24 15:36:04 +0900

    tqdm

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 from pylib.sc_util import *
 import time
+from tqdm import tqdm
 
 #%%
 def eval_f(value):
-    print("end", end=" ")
-    for i in range(3):
-        time.sleep(10)
-        print(i, end=" ")
+    # print("end", end=" ")
+    for i in tqdm(range(100)):
+        time.sleep(0.05)
+        # print(i, end=" ")
     return len(value)
 
 
