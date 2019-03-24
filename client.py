8fb5792fae5c66f62b335a29d06a37810d4eb43d
ctare <sankaku900622@yahoo.co.jp>
2019-03-24 15:31:39 +0900

    sleep 10

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 from pylib.sc_util import *
+import time
 
 #%%
 def eval_f(value):
     print("end", end=" ")
     for i in range(3):
-        time.sleep(1)
+        time.sleep(10)
         print(i, end=" ")
     return len(value)
 
