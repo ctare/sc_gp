1a5b0602289de6a89d3372524ac13c4bc1f74ebb
ctare <sankaku900622@yahoo.co.jp>
2019-04-03 19:04:49 +0900

    learning

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 from pylib.sc_util import *
 from pylib.gp_util import *
 from pylib.hgp import *
+import json
 
 #%%
 # globals().update(load_data("jordan_ok/"))
@@ -12,13 +13,17 @@ phase_init()
 
 #%%
 def eval_f(value):
-    print("end", end=" ")
-    for i in range(3):
-        time.sleep(0.01)
-        print(i, end=" ")
-    # return value ** 2
-    return -len(value)
-    # return eval_simple_tree(value)
+    # print("end", end=" ")
+    # for i in range(3):
+    #     time.sleep(0.01)
+    #     print(i, end=" ")
+    # # return value ** 2
+    # return -len(value)
+    print("learning...   model size ->", len(value))
+    data = json.loads(value)
+    tree = data["tree"]
+    penalty = data["penalty"]
+    return eval_simple_tree(tree) * penalty
 
 
 #%%
