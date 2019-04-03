6d0c2ebec9d90680e4d81ad56831217521f66744
ctare <sankaku900622@yahoo.co.jp>
2019-04-03 21:01:29 +0900

    save tree image

--- client.py
+++ client.py
@@ -15,7 +15,10 @@ creator_init()
 phase_init()
 
 #%%
+evcnt = 0
 def eval_f(value):
+    global evcnt
+    evcnt += 1
     # print("end", end=" ")
     # for i in range(3):
     #     time.sleep(0.01)
@@ -27,6 +30,27 @@ def eval_f(value):
     tree = data["tree"]
     penalty = data["penalty"]
     loss_v = eval_simple_tree(tree)
+
+    print("loss_v", loss_v)
+    individual = creator.Individual(parse_simple_tree(tree))
+    edges, nodes, labels = get_graph(individual, GlobalStat.pset)
+    g = nx.MultiDiGraph()
+    for i, node in enumerate(nodes):
+        # nodes[i] = (node, {"label": labels[node]})
+        if type(labels[node]) == tuple:
+            name, v = labels[node]
+            nodes[i] = (node, {"label": name, **v})
+        else:
+            nodes[i] = (node, {"label": labels[node]})
+
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+
+    agraph = nx.nx_agraph.to_agraph(g)
+    # agraph.node_attr["shape"] = "circle"
+    agraph.node_attr["fontsize"] = "20"
+    agraph.draw(f"tree_{evcnt}.png", prog="dot", format="png")
+
     print("loss", float(np.mean(loss_v[-5:])), "penalty", penalty)
     return float(np.mean(loss_v[-5:])) * penalty
 
