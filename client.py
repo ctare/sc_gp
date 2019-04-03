d8c2e6dcc1f59c65a46680479507f0f4e86fa595
ctare <sankaku900622@yahoo.co.jp>
2019-04-03 17:04:05 +0900

    to gp

--- client.py
+++ client.py
@@ -1,19 +1,24 @@
 from pylib.sc_util import *
-import time
-from tqdm import tqdm
-import json
+from pylib.gp_util import *
+from pylib.hgp import *
+
+#%%
+# globals().update(load_data("jordan_ok/"))
+
+#%%
+GlobalStat.reset()
+creator_init()
+phase_init()
 
 #%%
 def eval_f(value):
-    # print("end", end=" ")
-    data = json.loads(value)
-    string = data["string"]
-    phase = data["phase"]
-    print("phase", phase)
-    for i in tqdm(range(10)):
+    print("end", end=" ")
+    for i in range(3):
         time.sleep(0.01)
-        # print(i, end=" ")
-    return len(string)
+        print(i, end=" ")
+    # return value ** 2
+    return len(value)
+    # return eval_simple_tree(value)
 
 
 #%%
