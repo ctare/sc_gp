fe927f98c0a7e1712833286f6d2befe6cc0e0326
ctare <sankaku900622@yahoo.co.jp>
2019-04-04 20:07:03 +0900

    揉み消し

--- client.py
+++ client.py
@@ -19,41 +19,36 @@ evcnt = 0
 def eval_f(value):
     global evcnt
     evcnt += 1
-    # print("end", end=" ")
-    # for i in range(3):
-    #     time.sleep(0.01)
-    #     print(i, end=" ")
-    # # return value ** 2
-    # return -len(value)
-    print("learning...   model size ->", len(value))
-    data = json.loads(value)
-    tree = data["tree"]
-    penalty = data["penalty"]
-    with open("/root/share/tree_test.txt", "w") as f:
-        f.write(json.dumps(tree))
-    individual = creator.Individual(parse_simple_tree(tree, GlobalStat.pset, GlobalStat.phase))
-    edges, nodes, labels = get_graph(individual, GlobalStat.pset)
-    g = nx.MultiDiGraph()
-    for i, node in enumerate(nodes):
-        # nodes[i] = (node, {"label": labels[node]})
-        if type(labels[node]) == tuple:
-            name, v = labels[node]
-            nodes[i] = (node, {"label": name, **v})
-        else:
-            nodes[i] = (node, {"label": labels[node]})
-
-    g.add_nodes_from(nodes)
-    g.add_edges_from(edges)
-
-    agraph = nx.nx_agraph.to_agraph(g)
-    # agraph.node_attr["shape"] = "circle"
-    agraph.node_attr["fontsize"] = "20"
-    agraph.draw(f"/root/share/tree_{evcnt}.png", prog="dot", format="png")
+    # print("learning...   model size ->", len(value))
+    # data = json.loads(value)
+    # tree = data["tree"]
+    # penalty = data["penalty"]
+    # with open("/root/share/tree_test.txt", "w") as f:
+    #     f.write(json.dumps(tree))
+    # individual = creator.Individual(parse_simple_tree(tree, GlobalStat.pset, GlobalStat.phase))
+    # edges, nodes, labels = get_graph(individual, GlobalStat.pset)
+    # g = nx.MultiDiGraph()
+    # for i, node in enumerate(nodes):
+    #     # nodes[i] = (node, {"label": labels[node]})
+    #     if type(labels[node]) == tuple:
+    #         name, v = labels[node]
+    #         nodes[i] = (node, {"label": name, **v})
+    #     else:
+    #         nodes[i] = (node, {"label": labels[node]})
+    #
+    # g.add_nodes_from(nodes)
+    # g.add_edges_from(edges)
+    #
+    # agraph = nx.nx_agraph.to_agraph(g)
+    # # agraph.node_attr["shape"] = "circle"
+    # agraph.node_attr["fontsize"] = "20"
+    # agraph.draw(f"/root/share/tree_{evcnt}.png", prog="dot", format="png")
 
     try:
         loss_v = eval_simple_tree(tree)
     except:
-        exit()
+        print("exception!!!!")
+        loss_v = [1000] * 5
 
     print("loss", float(np.mean(loss_v[-5:])), "penalty", penalty)
     return float(np.mean(loss_v[-5:])) * penalty
